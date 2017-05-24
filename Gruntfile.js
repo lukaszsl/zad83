a543e4aa690239885d4b480d7e128faacd5b80e4
Lukasz Sliczner <lukaszsliczner@MacBook-Pro.local>
2017-05-24 20:42:13 +0000

    conected with gitHub

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+// This shows a full config file!
+module.exports = function (grunt) {
+    grunt.initConfig({
+        watch: {
+            files: 'sass/**/*.sass',
+            tasks: ['sass']
+        },
+        sass: {
+            dev: {
+                files: {
+                    'css/style.css': 'sass/style.sass'
+                }
+            }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'css/*.css',
+                        '*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './'
+                }
+            }
+        }
+    });
+
+    // load npm tasks
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+    // define default task
+    grunt.registerTask('default', ['sass', 'browserSync', 'watch']);
+};
